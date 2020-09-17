633457881e00eec7a3bbf3a2371cb11ee74a5d86
liaowenqiang828 <18840965351@163.com>
2020-09-17 22:07:33 +0800

    [Wenqiang Liao] feat: calculate the days between any date to the next labor day

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -9,8 +11,14 @@ import java.time.LocalDate;
  * @create 2020-05-15_16:33
  */
 public class Practice1 {
-
+  private static int Year = LocalDate.now().getYear();
+  private static final LocalDate CURRENT_LABOR_DAY = LocalDate.of(Year, 5,1);
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    if (date.isAfter(CURRENT_LABOR_DAY)) {
+      LocalDate nextLaborDay = CURRENT_LABOR_DAY.plusYears(1);
+      return ChronoUnit.DAYS.between(date, nextLaborDay);
+    }
+
+    return ChronoUnit.DAYS.between(date, CURRENT_LABOR_DAY);
   }
 }
