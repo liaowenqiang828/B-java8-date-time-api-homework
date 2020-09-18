9da88387ac23bd348c1642a45246febd4c615ebf
liaowenqiang828 <18840965351@163.com>
2020-09-18 16:14:53 +0800

    [Wenqiang Liao] refactor: extract constant

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -10,15 +10,18 @@ import java.time.LocalDate;
  * @create 2020-05-15_17:20
  */
 public class Practice2 {
+  private static final int NEXT_WORKDAY_NEAR_FRIDAY = 3;
+  private static final int NEXT_WORKDAY_NEAR_SATURDAY = 2;
+  private static final int NEXT_WORKDAY = 1;
 
   public static LocalDate getNextWorkDate(LocalDate date) {
     DayOfWeek dayOfWeek = date.getDayOfWeek();
     if (dayOfWeek == DayOfWeek.FRIDAY) {
-      return date.plusDays(3);
+      return date.plusDays(NEXT_WORKDAY_NEAR_FRIDAY);
     }
     if (dayOfWeek == DayOfWeek.SATURDAY) {
-      return date.plusDays(2);
+      return date.plusDays(NEXT_WORKDAY_NEAR_SATURDAY);
     }
-    return date.plusDays(1);
+    return date.plusDays(NEXT_WORKDAY);
   }
 }
